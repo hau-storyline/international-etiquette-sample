b0dbbf59703cd7e7560fd6fa584f976445d7d542
hau-storyline <bichhau1213@gmail.com>
2024-07-08 05:45:38 +0700

    storyline sample
    storyline sample

---
+++ story_content/5abvJ5fTMry_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.85.31840.0%0D%0A%0D%0A00:00:00.000%20--%3E%2000:00:04.356%0D%0ACongratulations!%20You\u0027ve%20completed%20the%20lesson%20on%20International%20Etiquette.%0D%0A%0D%0A"}')
---
+++ story_content/6L6J4px80xh_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.85.31840.0%0D%0A%0D%0A00:00:00.064%20--%3E%2000:00:01.959%0D%0AClick%20on%20the%20play%20button%20to%20learn%20more.%0D%0A%0D%0A"}')
